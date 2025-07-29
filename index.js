e254c818ad0ef6e6a84cff4c4cce74cab62f0a41
FernandoRabeloGoncalves <fernandorabelogoncalves@hotmail.com>
2025-07-29 00:20:08 -0300

    Meu Segundo Commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log ("Olá Mundo")
+let pontosDeVida = 0
+
+for (let i = 0; i < 10; i++) {
+    pontosDeVida += 1
+    console.log ("Tomou poção mágica" + i)
+}
+
+console.log (pontosDeVida + " Totais ")
