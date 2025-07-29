5f313fc6abef3fb7f8ec5ba83f577d1b13dea9b3
FernandoRabeloGoncalves <fernandorabelogoncalves@hotmail.com>
2025-07-29 00:53:56 -0300

    Desafio do Felipão Concluido

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let pontosDeVida = 0
+let nome = "Lord Knight";
+let xp = 50000; 
+let nivel = "";
 
-for (let i = 0; i < 10; i++) {
-    pontosDeVida += 1
-    console.log ("Tomou poção mágica" + i)
+if (xp <= 1000) {
+    nivel = "Ferro";
+}
+else if (xp <= 2000) {
+    nivel = "Bronze";
+}
+else if (xp <= 5000) {
+    nivel = "Prata";
+}
+else if (xp <= 6000) {
+    nivel = "Ouro";
+}
+else if (xp <= 7000) {
+    nivel = "Platina Diamante";
+}
+else if (xp <= 8000) {
+    nivel = "Ascendente";
+}
+else if (xp <= 9000) {
+    nivel = "Imortal";
+}
+else {
+    nivel = "Radiante";
 }
 
-console.log (pontosDeVida + " Totais ")
+console.log("Nosso Herói Chamado " + nome + " está no nível de " + nivel + " com " + xp + " de experiência.");
